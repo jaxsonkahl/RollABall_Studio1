b352bf1502dedb5d0e9a08bc10558924fae8d975
Jaxson Kahl <jaxsonkahl@gmail.com>
2025-01-26 15:38:00 -0800

    added rigid body to ball controller

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,7 +2,8 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody sphereRigidbody;
+    
     void Start()
     {
         Debug.Log("Calling the Start method");
@@ -11,6 +12,27 @@ public class BallController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Calling the Update method");
+        Vector2 inputVector = Vector2.zero; // intialize our input vector
+        if (Input.GetKey(KeyCode.W))
+        {
+            inputVector += Vector2.up; // "a += b" <=> "a = a + b"
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector += Vector2.down;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector += Vector2.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            inputVector += Vector2.left;
+        }
+
+        Debug.Log("Resultant Vector: " + inputVector);
     }
 }
